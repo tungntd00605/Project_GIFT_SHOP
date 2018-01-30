79a38f95721172536b153cd27f2d306165cdd289
tungntd00605 <tungntd00605@fpt.edu.vn>
2018-01-30 17:08:27 +0700

    Thêm trang product-list, sắp xếp lại code cho sạch, đổi tên style.css thành header.css

--- js/product-home.js
+++ js/product-home.js
@@ -22,7 +22,29 @@ $(document).ready(function() {
 	}
   })
 })
-
+$(document).ready(function() {
+  $('.owl-carousel-category').owlCarousel({
+	loop: true,
+	margin: 10,
+	responsiveClass: true,
+	responsive: {
+	  0: {
+		items: 2,
+		nav: true
+	  },
+	  768: {
+		items: 6,
+		nav: true
+	  },
+	  1000: {
+		items: 10,
+		nav: true,
+		loop: true,
+		margin: 1,
+	  }
+	}
+  })
+})
 $(document).ready(function() {
   $('.owl-carousel-popular').owlCarousel({
 	loop: true,
